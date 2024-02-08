682c3781da4493410703d63e1866bdf32f9de241
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2024-02-08 11:03:08 -0600

    Fix error after change identity in icp-deploy.sh

--- scripts/icp/icp-deploy.sh
+++ scripts/icp/icp-deploy.sh
@@ -2,9 +2,13 @@
 
 SCRIPTS_DIR=$PWD/scripts
 
+CURRENT_IDENTITY_NAME=$(dfx identity whoami)
+
 dfx identity new minter
 dfx identity use minter
 export ICP_MINTER_ACCOUNT_ID=$(dfx ledger account-id)
+
+dfx identity use $CURRENT_IDENTITY_NAME
 
 export ICP_DEFAULT_ACCOUNT_ID=$(dfx ledger account-id --of-principal "2vxsx-fae")
 
